751474c5d26e9b7900c59666263107741bfc9d2c
PropaNice <ollyJTD@gmail.com>
2025-09-26 11:09:49 +0100

    Increments and Decrements
    Added new line using increments and decrements +-/x-+
    
    Added comments to explain how these work and how placement can affect variables state.

--- Tasks/01 - Variables and Arithmetic/105-WritingToTheConsoleWithC++/105-WritingToTheConsoleWithC++.cpp
+++ Tasks/01 - Variables and Arithmetic/105-WritingToTheConsoleWithC++/105-WritingToTheConsoleWithC++.cpp
@@ -10,15 +10,28 @@ int main()
     cout << "Hello World!" << endl;   // Write Hello World to the terminal
 
     // ***************** Using cout with newlines ************************
-    cout << "Hello Everyone.We are using C++\nWe can still use \\n?\n";
+    cout << "Hello Everyone. We are using C++\nWe can still use \\n\n";
 
     // ********************** Setting Precision **************************
-    int age = 23;
+    int age = 25;
     cout << "My age is  " << age << endl;
-
+    cout << "Next year my age will be " << ++age << endl;
+/* my entry point, 
+    ++ allows increment by the value by one before using it
+    -- allows decrement of the value
+    
+    There is a trick here, the posistioning of ++x or x++ 
+    determines how the variable will be affected. For example, 
+    if you use age++ it will increment the value of the variable,
+    but not update the value until after the line is executed.
+    This means that "age" will remain as 25, but instead if we used ++age 
+    it will increment the value and maintain the new value stored in the variable.
+    */
+    
     // Setting precision is not so easy?
     float radius = 10.0f;
-    float area = 3.415926 * radius * radius;
+    float area = 3.415926 * radius * radius;  
+    // My entry point, area of circle = pi * r^2 
 
     cout << fixed;
     cout << setprecision(1);
