031c718b6d780b2386de57ffb44ae74ff8f2a57f
PropaNice <ollyJTD@gmail.com>
2025-09-26 10:22:34 +0100

    Completed Tasks (b), (c)
    Task (b) Ask me to output to variable "area" to 3 decimal places.
    
    I achieved this by using "%.Xf"
    where X is equal to number decimal to places to output.
    
    Task (c) Asks me to output the values of 2 variable "area" and "radius" in a single printf statement
    
    I achieved this by separating each variable with commas and calling each of the variables respectively.

--- Tasks/01 - Variables and Arithmetic/101-WritingToTheConsole/103-WritingToTheConsole.cpp
+++ Tasks/01 - Variables and Arithmetic/101-WritingToTheConsole/103-WritingToTheConsole.cpp
@@ -23,10 +23,22 @@ int main()
     float radius = 10.0f;
     float area = 3.415926 * radius * radius;
 
-    // TASK(b) - Use printf to display the value of the variable area. Look at the lecture slides and find out how to limit this to 3 decimal places
+    // TASK(b) - Use printf to display the value of the variable area. 
+    // Look at the lecture slides and find out how to limit this to 3 decimal places
     // Write solution here
     
-    //
+    /* This my entry point  
+
+    Response to Task (b): To limit the output of the variable area to 3 decimal places,
+    In order to output the variable area to 3 decimal points we have to use 
+    (%.xf) where x is the number of decimal points we want to display.
+    */
+    
+    printf("The area is %.3f\n", area);
+   
+    /* we call the printf function with the string, we also call the variable area 
+    and limit the float value to 3 decimal places using %.3f.
+    */
 
     // You can output complex strings to the terminal
     int p = 3;
@@ -35,8 +47,19 @@ int main()
 
     // TASK(c) - using printf, display the radius and the area using a single printf statement
     // Write solution here
-
-    //
+    
+    /* This my entry point
+    
+    Response to Task (c): To display the radius and area using a single printf statement,
+    to display multiple variables in a single printf statement we can use multiple
+    calls for variables in the string. 
+    */
+    
+    printf("radius=%.3f, area=%.3f\n", radius, area);
+    // we call the printf function with the string, we use %f to display the values as float.
+    // we also call the variables radius and area.
+    // we use comma to separate the variables in the call.
+    // like the previous task we limit the float values to 3 decimal places using %.3f.
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
