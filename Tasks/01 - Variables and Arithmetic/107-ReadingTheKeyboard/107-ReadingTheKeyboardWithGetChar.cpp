6aa19aa5f2f24e56f66d490880a449ad83acfa26
PropaNice <ollyJTD@gmail.com>
2025-09-27 22:07:23 +0100

    comments to 107

--- Tasks/01 - Variables and Arithmetic/107-ReadingTheKeyboard/107-ReadingTheKeyboardWithGetChar.cpp
+++ Tasks/01 - Variables and Arithmetic/107-ReadingTheKeyboard/107-ReadingTheKeyboardWithGetChar.cpp
@@ -8,8 +8,17 @@ int main()
     //Read the keyboard and copy the ASCII code into c
     c = getchar();
 
+    // Prompt user for input
+    printf("Please input a single character\n");
+    
     //Write the decimal and ASCII representation to the terminal
     printf("You typed %c\n", c);
     printf("This has ASCII code %d\n", c);
 
+
+    // This is my entry point
+    // when the program executes getchar() will listen to for the first input from the user,  
+    // %c will output the character the user inputs
+    // %d will then output entered character denoted by its ASCII code
+    // This because we are outputing a character as a decimal which gives us its corresponding ASII code
 }
