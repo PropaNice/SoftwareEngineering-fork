58be6a56dc6aa8771a82fd36a848b74f59f1b22c
PropaNice <ollyJTD@gmail.com>
2025-09-27 22:11:54 +0100

    Added print statement , prompting the user to enter a single character

--- Tasks/01 - Variables and Arithmetic/107-ReadingTheKeyboard/107-ReadingTheKeyboardWithGetChar.cpp
+++ Tasks/01 - Variables and Arithmetic/107-ReadingTheKeyboard/107-ReadingTheKeyboardWithGetChar.cpp
@@ -2,23 +2,22 @@
 
 int main()
 {
+    // Prompt user for input
+    printf("Please input a single character\n");
+
     //Create a variable of type char (8-bit integer)
     char c = 0;
     
     //Read the keyboard and copy the ASCII code into c
     c = getchar();
 
-    // Prompt user for input
-    printf("Please input a single character\n");
-    
     //Write the decimal and ASCII representation to the terminal
     printf("You typed %c\n", c);
     printf("This has ASCII code %d\n", c);
-
+}
 
     // This is my entry point
     // when the program executes getchar() will listen to for the first input from the user,  
     // %c will output the character the user inputs
     // %d will then output entered character denoted by its ASCII code
     // This because we are outputing a character as a decimal which gives us its corresponding ASII code
-}
